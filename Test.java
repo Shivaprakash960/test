3fc5d56641c0f0ec79bcc9c799a679d342319f1e
Shivaprakash960 <113585078+Shivaprakash960@users.noreply.github.com>
2022-10-13 14:22:02 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,108 @@
+1)The meaning of platform-independent is that the java compiled code can run on all java. 
+A program is written in a language that is a human-readable language. 
+It may contain words, phrases which the machine cannot understand.
+
+
+
+2) primitive and non primitive data types
+primitive types are :-
+	String and ClassName 
+non primitive data types are:-
+	byte,int,char,boolaen,long,short,double,float.
+In primitive data types the it starts with the upper case. 
+In non primitive data types it starts with the lowwer case. 
+data types are n number.
+data types are only 8 types.
+
+
+3) class and its member 
+class:-class is design which consist of states and behvariers.
+	in class we have 
+	UserName
+	varibles
+	declartion 
+	methods
+eg of class:-
+class User{
+
+	public static void main(String []a){
+	int a=1;
+	System.out.println(a);
+	
+	}
+
+}
+
+4)program
+class Calculation{
+
+	public static void main(String [] arg){
+	int totalMarks=600;
+	int receivedMarks=500;
+	
+	double percentage=((receivedMarks/totalMarks)*100);
+	
+	System.out.println(percentage);
+		return;
+	}
+}
+
+5)program
+class Array{
+
+	public static void main(String [] arg){
+	
+	int[] values={34,78,90,53,34,11,53,15,78,91,8,4,67,11};
+	
+	if(i=i){}
+	for(i=0;i<values.length;i++){
+		System.out.println(i);
+	}
+	}
+
+}
+
+6)main method is method programruns. when is execuets the with respctive to method 
+the program will be execuets in run time.
+method syntix:
+acsses,non-acess,returntype,mainmethod(parametares){
+
+} 
+
+7)reverse number
+class Reverse{
+
+	public static void main(String [] arg){
+	int number=14675,reverse=0;
+	int reminder;
+	
+	while(number>0){
+	reminder = number%10;
+	   number = number/10;
+	   reverse = (reverse * 10)+ reminder;
+	   
+	  
+	 }
+	  System.out.println(reverse);
+	
+	
+	
+	}
+}
+
+8)hours into seconds
+class Seconds{
+
+	public static void main(String [] agr){
+	int hours=2;
+	int seconds=(hours*60*60);
+	
+	System.out.println(seconds);
+	
+	
+	}
+
+
+}
+
+9)
